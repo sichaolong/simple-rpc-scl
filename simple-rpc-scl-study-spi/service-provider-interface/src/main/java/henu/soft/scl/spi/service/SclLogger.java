23496f9fb1822eb6e3db035807c305abcd3e2acb
GitHubSi <2589165806@qq.com>
2022-09-04 23:33:36 +0800

    add simple-rpc-scl-study-spi model and update readmefile...

---
+++ simple-rpc-scl-study-spi/service-provider-interface/src/main/java/henu/soft/scl/spi/service/SclLogger.java
@@ -0,0 +1,8 @@
+package henu.soft.scl.spi.service;
+
+/**
+ * @author sichaolong
+ * @date 2022/9/4 22:05
+ */
+public class SclLogger {
+}
---
+++ simple-rpc-scl-study-spi/service-provider/src/main/java/henu/soft/scl/spi/provider/SclLogBack.java
@@ -0,0 +1,8 @@
+package henu.soft.scl.spi.provider;
+
+/**
+ * @author sichaolong
+ * @date 2022/9/4 22:27
+ */
+public class SclLogBack {
+}
